0ebbc5aa8c114dc02f08c69118794ec296dcecd6
paul lhungdim <paullhungdim7@gmail.com>
2021-01-27 21:05:06 +0530

    update

---
+++ precache-manifest.87fdd5316e07548c781eaf809afdc2c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9858fd4da40523347d3e",
+    "url": "/timetable/css/app.f21c8b68.css"
+  },
+  {
+    "revision": "cfb4cb67883251dae686679651cfea1f",
+    "url": "/timetable/index.html"
+  },
+  {
+    "revision": "9858fd4da40523347d3e",
+    "url": "/timetable/js/app.daf7d7c8.js"
+  },
+  {
+    "revision": "59b1cb36233a7eb3c590",
+    "url": "/timetable/js/chunk-vendors.c9abc56a.js"
+  },
+  {
+    "revision": "30878a20238e0f67d652ea2919e02f19",
+    "url": "/timetable/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/timetable/robots.txt"
+  }
+]);
